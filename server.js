e481ab0a4076a5175c30b56d48dca87917889b03
Dharshan B <109328490+Dharshan-B@users.noreply.github.com>
2026-04-06 14:00:52 +0530

    Use environment variable for server port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const ExcelJS = require('exceljs');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
